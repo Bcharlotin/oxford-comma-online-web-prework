ca7584f1c23bbf1138ac7fc20d26c46be0d3bb23
biancacharlotin <biancacharlotin@gmail.com>
2018-11-12 15:22:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
- return array.join
-end
-
-def oxford_comma(array2)
-  return array2.join(" and ")
+  when array.length == 1
+    return array.join
+ 
+  when array.length == 2
+    return array2.join(" and ")
+    
+  when array.length == 3
+    return array2.join(" ,and ")
 end
